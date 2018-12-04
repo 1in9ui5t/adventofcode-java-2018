d77c7b12116d9f81c0993683c166cdfd7e67d977
Shaheen Georgee <sgeorgee@seamgen.com>
2018-12-04 00:41:52 -0800

    * Advent of Code 2018: Day 2 Part 2 - O(n^2) approach

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -1,11 +1,10 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Day2 {
     public static void main(String[] args) {
         List<String> boxIds = PuzzleInput.getInputForDay(2);
         System.out.println("Result of Day 2, Part 1: " + generateChecksum(boxIds));
+        System.out.println("Result of Day 2, Part 2: " + findCommonLettersInFabricBoxesIds(boxIds));
     }
 
     private static int generateChecksum(List<String> input) {
@@ -20,7 +19,7 @@ public class Day2 {
                 } else {
                     occurrences++;
                 }
-                charactersOccurrence.put(new Integer(letter), occurrences);
+                charactersOccurrence.put(letter, occurrences);
             });
             boolean hasExactlyTwoOfSomeLetter = false;
             boolean hasExactlyThreeOfSomeLetter = false;
@@ -43,5 +42,31 @@ public class Day2 {
             }
         }
         return countOfTwos * countOfThrees;
+    }
+
+    private static String findCommonLettersInFabricBoxesIds(List<String> input) {
+        String commonLetters = null;
+        int i, j;
+        for (i = 0; i < input.size() && commonLetters == null; i++) {
+            for (j = i; j < input.size(); j++) {
+                commonLetters = getCommonLettersIfOffByOneCharacter(input.get(i), input.get(j));
+                if (commonLetters != null) {
+                    break;
+                }
+            }
+        }
+        return commonLetters;
+    }
+
+    private static String getCommonLettersIfOffByOneCharacter(String target, String other) {
+        int variations = 0;
+        int variationIndex = -1;
+        for (int i = 0; variations <= 1 && i < target.length(); i++) {
+            if (target.charAt(i) != other.charAt(i)) {
+                variations++;
+                variationIndex = i;
+            }
+        }
+        return variations == 1 ? new StringBuilder(target).deleteCharAt(variationIndex).toString() : null;
     }
 }
