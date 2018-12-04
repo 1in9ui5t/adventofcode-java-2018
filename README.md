8873c8c7c98a15aa655bc4677edd72358f4ecf7a
Shaheen Georgee <sgeorgee@seamgen.com>
2018-12-03 16:02:33 -0800

    * Advent of Code 2018: Day 1 puzzles in Java

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2018
+
+Checkout the details at https://adventofcode.com/2018
+
+## Goals
+
+1. Complete 50 brain teasers in the month of December
+2. Utilize best Java language constructs available (adopt and highlight constructs new to me along the way)
+3. Analyze resources allocation of each solution and attempt to optimize 
---
+++ src/main/java/Day1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Day1 {
+    public static void main(String[] args) {
+        List<Integer> frequencyDeltas = PuzzleInput.getFrequencyDeltas();
+        System.out.println("Result of Day 1, Part 1: " + sum(frequencyDeltas));
+        System.out.println("Result of Day 1, Part 2: " + detectFirstDuplicateFrequencyLooparound(frequencyDeltas));
+    }
+
+    private static int sum(List<Integer> input) {
+        return input.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private static Integer detectFirstDuplicateFrequencyLooparound(List<Integer> input) {
+        int currentSum = 0;
+        Integer delta = currentSum;
+        Map<Integer, Boolean> resultingFrequencies = new HashMap<>();
+        resultingFrequencies.put(new Integer(currentSum), true);
+        int index = 0;
+        int loopsAround = 0;
+        do {
+            resultingFrequencies.put(new Integer(currentSum), true);
+            delta = input.get(index);
+            currentSum += delta;
+            if (resultingFrequencies.containsKey(new Integer(currentSum))) {
+                System.out.println("Duplicate frequency encountered after " + loopsAround + " loops of the input list");
+                return currentSum;
+            }
+            if (++index == input.size()) {
+                index = 0;
+                loopsAround++;
+            }
+        } while (true);
+    }
+}
